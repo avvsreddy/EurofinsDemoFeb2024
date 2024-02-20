85f2dad7f8e3c474ad0a9b812e41d4dbecd77886
Venkat Shiva Reddy <venkatshiva.reddy@gmail.com>
2024-02-20 16:31:11 +0530

    oo

---
+++ DataTypesDemo/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace DataTypesDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            int[] a = new int[10];
+            int[] b = new int[3] { 1, 2, 3 };
+            int[] c = new int[] { 1, 2, 3 };
+            int[] d = { 1, 3, 4 };
+
+            //Sum(10, 20);
+            Sum("", b);
+            Sum("", 10, 20, 30);
+            Sum("", 10, 20, 30, 40);
+            Sum("", 10);
+            Sum("");
+
+            //Print("resume",1,"gray");
+
+            Print("resume", color: "gray", printer: "ltp1");
+            Print(printer: "lpts", doc: "invoice");
+        }
+
+
+        static Tuple<int, int> SumMax(int a, int b)
+        {
+            int m = a > b ? a : b;
+            int s = a + b;
+            Tuple<int, int> t = new Tuple<int, int>(m, s);
+            return (t);
+            int sum;
+            int max;
+            SumMax2(10, 20, out max, out sum);
+            Console.WriteLine(sum);
+            Console.WriteLine(max);
+        }
+
+        static void SumMax2(int a, int b, out int m, out int s)
+        {
+            //Console.WriteLine(m);
+            //
+            //
+            m = a > b ? a : b;
+            s = a + b;
+            //Tuple<int, int> t = new Tuple<int, int>(m, s);
+            //return (t);
+        }
+
+        static int Sum(string str, params int[] numbers)
+        {
+            return a + b + c + d;
+        }
+
+        static void Print(string doc, int copies = 1, string color = "black", string printer = "sd")
+        {
+
+        }
+
+    }
+}
---
+++ OODemo1/Program.cs
@@ -0,0 +1,115 @@
+﻿using System;
+
+namespace OODemo1
+{
+    internal class Program
+    {
+
+        //var name;
+        //Employee e = new Employee(); // Has - a
+        static void Main(string[] args)
+        {
+            // var salary =  GetSalary(111);
+            // var a = 10;
+            // a = "234";
+            var str = "sdfsdfsdf";
+
+
+            var p2 = new { ProdudctID = 2 }; // Anonymous types
+
+            var p1 = new { ProdudctID = 1, Name = "Iphone", Price = 10000, Brand = "Apple", Category = "Mobiles" };
+
+            var p3 = new { ProdudctID = 3, Name = "IPhione 15 Pro" };
+
+            var p4 = new { ProdudctID = 234, Rating = 5, Supplier = "abv" };
+            //p4.Rating = 5;
+
+            Console.WriteLine(p4.Supplier);
+
+            // Object Initialization Syntax
+
+
+            //p1.ProdudctID = 1;
+            //p1.Name = "IPhone ";
+            //p1.Brand = "Apple";
+            //p1.Price = 100000;
+            //p1.Category = "Mobiles";
+
+        }
+    }
+
+    class Employee //: Program // IS-A
+    {
+        // state / fields
+        private int eid;
+        //private string name;
+        //private string _backing34234234234;
+        private int lname;
+        private int salary;
+
+
+        public Employee(string n)
+        {
+            Name = n;
+        }
+        public string Name
+        {
+            get;// { return _backing34234234234; }
+            private set;// { _backing34234234234 = value; }
+        }
+
+        public int Eid
+        {
+            get { return eid; }
+            set { eid = value; }
+        }
+
+        public int Salary  // Property
+        {
+            set
+            {
+                if (value < 1000)
+                {
+                    salary = 1000;
+                }
+                else
+                    salary = value;
+            }
+            get
+            {
+                return salary;
+            }
+        }
+
+    }
+
+
+    //class Product
+    //{
+    //    public int ProdudctID { get; set; }
+    //    public string Name { get; set; }
+    //    public int Price { get; set; }
+    //    public string Brand { get; set; }
+    //    public string Category { get; set; }
+
+    //    //public Product(int id)
+    //    //{
+    //    //    this.ProdudctID = id;
+    //    //}
+
+    //    //public Product(int id, string name):this(id)
+    //    //{
+    //    //    //this.ProdudctID=id;
+    //    //    this.Name= name;
+    //    //}
+
+    //    //public Product(int id, string name, int price, string brand, string category):this(id,name)
+    //    //{
+    //    //    //this.ProdudctID = id;
+    //    //    //this.Name = name;
+    //    //    this.Brand = brand;
+    //    //    this.Price = price;
+    //    //    this.Category = category;
+    //    //}
+    //}
+}
