1b1ef5a49b27523cfef2c8fecadecb9045afc579
Venkat Shiva Reddy <venkatshiva.reddy@gmail.com>
2024-02-26 15:52:55 +0530

    trainer-trainee-app-completed code

--- TrainerTraineeApp/Program.cs
+++ TrainerTraineeApp/Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 
 namespace TrainerTraineeApp
 {
@@ -6,12 +7,65 @@ namespace TrainerTraineeApp
     {
         static void Main(string[] args)
         {
+            Training training = new Training();
+            Trainer trainer = new Trainer();
+            training.TheTrainer = trainer;
+            Organization org = new Organization();
+            org.Name = "Pratian Technologies";
+            trainer.Org = org;
+
+            Console.WriteLine($"Org Name : {training.GetTrainingOrgName()}");
+
+
+            Trainee t1 = new Trainee();
+            Trainee t2 = new Trainee();
+            Trainee t3 = new Trainee();
+            Trainee t4 = new Trainee();
+            Trainee t5 = new Trainee();
+
+            training.Trainees.Add(t1);
+            training.Trainees.Add(t2);
+            training.Trainees.Add(t3);
+            training.Trainees.Add(t4);
+            training.Trainees.Add(t5);
+
+
+
+            Console.WriteLine($"Trainees Count : {training.GetNumOfTrainees()}");
+
+
+
+            Course course = new Course();
+            training.TheCourse = course;
+            Module m1 = new Module();
+            Module m2 = new Module();
+            course.Modules.Add(m1);
+            course.Modules.Add(m2);
+
+            Unit u1 = new Unit { Duration = 10 };
+            m1.Units.Add(u1);
+            Unit u2 = new Unit { Duration = 60 };
+            m1.Units.Add(u2);
+            Unit u3 = new Unit { Duration = 30 };
+            m2.Units.Add(u3);
+            Unit u4 = new Unit { Duration = 20 };
+            m2.Units.Add(u4);
+
+
+            Console.WriteLine($"Training Duration  : {training.GetTrainingDurationInHourse()}");
+
         }
     }
 
     class Organization
     {
         public string Name { get; set; }
+
+        //private string name;
+        //public GetName()
+        //{
+        //    return name;
+        //}
     }
     class Trainer
     {
@@ -33,19 +87,29 @@ namespace TrainerTraineeApp
         public int GetNumOfTrainees()
         {
             //throw new System.NotImplementedException();
-            int traineesCount = 0;
+            int traineesCount = Trainees.Count;
             // find the count
             return traineesCount;
         }
 
         public string GetTrainingOrgName()
         {
-            return null;
+            return TheTrainer.Org.Name;
         }
 
         public int GetTrainingDurationInHourse()
         {
-            return 0;
+            int totalDuration = 0;
+            // for each module in course
+            foreach (Module m in TheCourse.Modules)
+            {
+                // for each unit in a module
+                foreach (Unit unit in m.Units)
+                {
+                    totalDuration += unit.Duration;
+                }
+            }
+            return totalDuration;
         }
     }
     class Course
