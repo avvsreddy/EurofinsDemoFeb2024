628704946ff2b0974a8ae5997241f5213470cfb0
Venkat Shiva Reddy <venkatshiva.reddy@gmail.com>
2024-02-26 14:41:24 +0530

    oo labs

---
+++ HasADemos/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace HasADemos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Car car = new Car();
+            PetrolEngine p = new PetrolEngine();
+            DieselEngine d = new DieselEngine();
+            GasEngine g = new GasEngine();
+            car.engine = d;
+            car.Move();
+        }
+    }
+
+
+    public class Car
+    {
+        public Engine engine { get; set; }
+        public void Move()
+        {
+            engine.Start();
+        }
+    }
+    abstract public class Engine
+    {
+        abstract public void Start();
+    }
+
+    public class PetrolEngine : Engine
+    {
+        public override void Start()
+        {
+            Console.WriteLine("Starting Petrol Engine");
+        }
+    }
+
+    public class DieselEngine : Engine
+    {
+        public override void Start()
+        {
+            Console.WriteLine("Starting Diesel Engine");
+        }
+    }
+
+    public class GasEngine : Engine
+    {
+        public override void Start()
+        {
+            Console.WriteLine("Starting Gas Engine");
+        }
+    }
+}
---
+++ OneToManyHasADemo/Program.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+
+namespace OneToManyHasADemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // static/fixed
+
+            int[] numbers = new int[10];
+            numbers[0] = 1; // add
+            int data = numbers[0]; //read
+
+            // dyanamic collection
+            List<int> list = new List<int>();
+            list.Add(10); // add
+            data = list[0]; //read
+        }
+    }
+
+    class Company
+    {
+        Store[] stores = new Store[10];
+        List<Customer> customerList = new List<Customer>();
+        public Address Address { get; set; }
+    }
+    class Customer
+    {
+
+    }
+    class Store
+    {
+
+    }
+    class Address { }
+}
