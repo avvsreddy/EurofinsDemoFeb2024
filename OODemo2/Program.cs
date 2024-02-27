e8751de974a5a4570a941b923c6f5c633d60ce45
Venkat Shiva Reddy <venkatshiva.reddy@gmail.com>
2024-02-27 10:43:49 +0530

    ide lab

---
+++ OODemo2/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace OODemo2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Base class ref variable can hold derived type objects or it own objects - dynamic polymorphism
+            Shape s;// = new Shape();
+            s.Draw(); // early binding - static binding - compiler
+            //Circle c = new Circle();
+            s = new Circle(); // early
+            s.Draw();
+        }
+    }
+
+
+    //abstract class Shape
+    interface IShape
+    {
+        //public int x;
+        //public int y;
+        void Draw(); // non polymarphic
+        //{
+        //    Console.WriteLine("Drawing a Shape");
+        //}
+        double GetArea();
+        //{
+        //Console.WriteLine("Calculating Shape area");
+        //return x * y;
+        //}
+    }
+
+
+    class Triangle : IShape// Realization
+    {
+        public void Draw()
+        {
+            Console.WriteLine("Drawing Traiagle");
+        }
+
+        public double GetArea()
+        {
+            Console.WriteLine("Calculating Triangle area");
+            return .5 * 34 * 3;
+        }
+    }
+
+    class Circle : IShape // Generalization
+    {
+        public void Draw() //   hiding - non polymorphic
+        {
+            Console.WriteLine("Drawing a Circle");
+        }
+
+        public double GetArea()
+        {
+            return 34;
+        }
+    }
+}
