c3e2aa5098afa33652c07e0d00445bb190ab8aab
Venkat Shiva Reddy <venkatshiva.reddy@gmail.com>
2024-02-27 13:57:02 +0530

    OO Labs and references

--- IdeLab/Program.cs
+++ IdeLab/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace IdeLab
 {
@@ -10,11 +11,12 @@ namespace IdeLab
             LangJava java = new LangJava();
             LangCSharp cs = new LangCSharp();
             LangC c = new LangC();
+            Fortran f = new Fortran();
 
-            ide.Java = java;
-            ide.CSharp = cs;
-            ide.C = c;
-
+            ide.Languages.Add(java);
+            ide.Languages.Add(cs);
+            ide.Languages.Add(c);
+            ide.Languages.Add(f);
             ide.Work();
 
         }
@@ -22,49 +24,103 @@ namespace IdeLab
 
     class IDE
     {
-        public LangJava Java { get; set; }
-        public LangCSharp CSharp { get; set; }
-        public LangC C { get; set; }
+        //public LangJava Java { get; set; }
+        //public LangCSharp CSharp { get; set; }
+        //public LangC C { get; set; }
+
+        // List<ILanguage> languages = new List<ILanguage>();
+        public List<ILanguage> Languages { get; set; } = new List<ILanguage>();
+
 
         public void Work()
         {
-            Console.WriteLine(Java.GetName());
-            Console.WriteLine(Java.GetUnit());
-            Console.WriteLine(Java.GetParadigm());
+            foreach (var l in Languages)
+            {
+                Console.WriteLine(l.GetName());
+                Console.WriteLine(l.GetUnit());
+                Console.WriteLine(l.GetParadigm());
+                Console.WriteLine("----------------");
+            }
+            //Console.WriteLine("--------------");
+            //Console.WriteLine(CSharp.GetName());
+            //Console.WriteLine(CSharp.GetUnit());
+            //Console.WriteLine(CSharp.GetParadigm());
 
-            Console.WriteLine("--------------");
-            Console.WriteLine(CSharp.GetName());
-            Console.WriteLine(CSharp.GetUnit());
-            Console.WriteLine(CSharp.GetParadigm());
-
-            Console.WriteLine("--------------");
-            Console.WriteLine(C.GetName());
-            Console.WriteLine(C.GetUnit());
-            Console.WriteLine(C.GetParadigm());
+            //Console.WriteLine("--------------");
+            //Console.WriteLine(C.GetName());
+            //Console.WriteLine(C.GetUnit());
+            //Console.WriteLine(C.GetParadigm());
 
 
         }
 
     }
 
-    class LangJava
+
+    interface ILanguage
     {
-        public string GetName() { return "Java Language"; }
-        public string GetUnit() { return "Class"; }
-        public string GetParadigm() { return "Object Oriented"; }
-    }
-    class LangCSharp
-    {
-        public string GetName() { return "CSharp Language"; }
-        public string GetUnit() { return "Class"; }
-        public string GetParadigm() { return "Object Oriented"; }
-    }
-    class LangC
-    {
-        public string GetName() { return "C Language"; }
-        public string GetUnit() { return "Function"; }
-        public string GetParadigm() { return "Procedural Oriented"; }
+
+        string GetName();
+        string GetUnit();
+        string GetParadigm();
     }
 
 
+    abstract class OOLanguage : ILanguage
+    {
+        abstract public string GetName();
+
+
+        public string GetParadigm()
+        {
+            return "Object Oriented";
+        }
+
+        public string GetUnit()
+        {
+            return "Class";
+        }
+    }
+
+    abstract class ProceduralLanguage : ILanguage
+    {
+        abstract public string GetName();
+
+
+        public string GetParadigm()
+        {
+            return "Procedural Oriented";
+        }
+
+        public string GetUnit()
+        {
+            return "Function";
+        }
+    }
+
+    class LangJava : OOLanguage //, ILanguage
+    {
+        public override string GetName() { return "Java Language"; }
+        //public string GetUnit() { return "Class"; }
+        //public string GetParadigm() { return "Object Oriented"; }
+    }
+    class LangCSharp : OOLanguage, ILanguage
+    {
+        public override string GetName() { return "CSharp Language"; }
+        //public string GetUnit() { return "Class"; }
+        //public string GetParadigm() { return "Object Oriented"; }
+    }
+    class LangC : ProceduralLanguage, ILanguage
+    {
+        public override string GetName() { return "C Language"; }
+        //public string GetUnit() { return "Function"; }
+        //public string GetParadigm() { return "Procedural Oriented"; }
+    }
+
+    class Fortran : ProceduralLanguage, ILanguage
+    {
+        public override string GetName() { return "Fortran Language"; }
+        //public string GetUnit() { return "Function"; }
+        //public string GetParadigm() { return "Procedural Oriented"; }
+    }
 }
