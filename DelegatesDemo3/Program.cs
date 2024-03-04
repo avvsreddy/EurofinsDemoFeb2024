69c73e4990e1aa3bc68422769d2f42d8cb075228
Venkat Shiva Reddy <venkatshiva.reddy@gmail.com>
2024-03-04 14:06:17 +0530

    Delegates demos

---
+++ DelegatesDemo3/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DelegatesDemo3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> list = new List<int>() { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            // find the sum
+            int sum = list.Sum();
+            // find all the even numbers sum
+            Func<int, bool> filter = new Func<int, bool>(IsEven);
+            int evensSum = list.Where(filter).Sum();
+            int evensSum2 = list.Where(IsEven).Sum();
+
+            int evensSum3 = list.Where(delegate (int x)
+            {
+                return x % 2 == 0;
+            }).Sum();
+
+            int evensSum4 = list.Where((int x) =>
+            {
+                return x % 2 == 0;
+            }).Sum();
+
+            int evensSum5 = list.Where((int x) =>
+                 x % 2 == 0
+            ).Sum();
+
+            int evensSum6 = list.Where(x => x % 2 == 0).Sum();
+        }
+
+
+        public static bool IsEven(int x)
+        {
+            return x % 2 == 0;
+        }
+
+    }
+}
---
+++ DelegatesDemo4/Program.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Net.Mail;
+
+namespace DelegatesDemo4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // client dev 1
+            Account account = new Account();
+
+            account.Notify += Notification.SendMail;// subscribed for mail
+            account.Notify += Notification.SendSMS; // sub for sms
+            account.Notify -= Notification.SendMail; // unsub for mail
+
+            account.Notify += Notification.SendWA;
+
+            account.Deposit(1000);
+
+            //account.Subscribe(Notification.SendSMS);
+            //account.Subscribe(Notification.SendMail);
+
+
+            // account.Notify("Deposited $9999999999999999.00");
+
+            Console.WriteLine($"Balance : {account.Balance}");
+            account.Withdraw(500);
+            Console.WriteLine($"Balance : {account.Balance}");
+        }
+    }
+
+    // Dev 1
+    class Account // business class
+    {
+        public int Balance { get; set; }
+
+        public event NotifyDelegate Notify = null; //new NotifyDelegate(Notification.SendMail);
+
+
+        //public void Subscribe(NotifyDelegate notify)
+        //{
+        //    Notify += notify;
+        //}
+
+        //public void Unsubscribe(NotifyDelegate notify)
+        //{
+        //    Notify -= notify;
+        //}
+
+        public void Deposit(int amount)
+        {
+            Balance += amount;
+            //Console.WriteLine("Deposited");
+            // send email notification
+            string msg = $"{amount} is deposited";
+            //Notification.SendMail(msg);
+            //Notification.SendSMS(msg);
+            if (Notify != null)
+                Notify(msg);
+
+
+        }
+
+        public void Withdraw(int amount)
+        {
+            Balance -= amount;
+            //Console.WriteLine("Withdrawn");
+            // send email notification
+            string msg = $"{amount} is Withdrawn";
+            //Notification.SendMail(msg);
+            //Notification.SendSMS(msg);
+            if (Notify != null)
+                Notify(msg);
+
+        }
+    }
+
+    // dev 1
+    public delegate void NotifyDelegate(string str);
+
+
+    // Dev 2
+    class Notification
+    {
+        public static void SendMail(string msg)
+        {
+            SmtpClient client = new SmtpClient();
+            //client.Port = 465;
+            //client.Host = "smtp.gmail.com";
+            //client.Credentials = null; // user id and password
+
+            client.DeliveryMethod = SmtpDeliveryMethod.SpecifiedPickupDirectory;
+            client.PickupDirectoryLocation = "E:\\mails";
+
+            MailMessage message = new MailMessage("manager@XYZBank.com", "customer@mail.com");
+            //message.Bcc = "";
+            //message.From = "";
+            //message.To = "";
+            message.Subject = msg;
+            message.Body = msg;
+            //message.Attachments.Add("");
+
+            //client.Send(message);
+            Console.WriteLine($"Email: {msg}");
+        }
+
+        public static void SendSMS(string msg)
+        {
+            Console.WriteLine($"SMS : {msg}");
+        }
+
+        public static void SendWA(string msg)
+        {
+            Console.WriteLine($"Whats App {msg}");
+        }
+
+    }
+}
+
+
+
