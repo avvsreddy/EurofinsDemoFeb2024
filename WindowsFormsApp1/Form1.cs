27752124b4ac35a6ac9ddb82926b68384e42aee5
Venkat Shiva Reddy <venkatshiva.reddy@gmail.com>
2024-03-05 14:29:15 +0530

    mt demo lab

---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Drawing;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void DrawRedRectangles(object sender, EventArgs e)
+        {
+            // red rect
+            Graphics g = panel1.CreateGraphics();
+            Random rnd = new Random();
+
+            for (int i = 1; i <= 1000; i++)
+            {
+                int x = rnd.Next(panel1.Height);
+                int y = rnd.Next(panel1.Width);
+                g.DrawRectangle(Pens.Red, x, y, 20, 20);
+                Thread.Sleep(100);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            // blue rect
+            Graphics g = panel2.CreateGraphics();
+            Random rnd = new Random();
+
+            for (int i = 1; i <= 1000; i++)
+            {
+                int x = rnd.Next(panel2.Height);
+                int y = rnd.Next(panel2.Width);
+                g.DrawRectangle(Pens.Blue, x, y, 20, 20);
+                Thread.Sleep(100);
+            }
+        }
+    }
+}
