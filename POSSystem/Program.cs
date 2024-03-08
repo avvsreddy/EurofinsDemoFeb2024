8ad5fa05fda112eb952a6f6ab5170ad082dac924
Venkat Shiva Reddy <venkatshiva.reddy@gmail.com>
2024-03-08 11:01:06 +0530

    DP demo

--- POSSystem/Program.cs
+++ POSSystem/Program.cs
@@ -7,17 +7,63 @@ namespace POSSystem
     {
         static void Main(string[] args)
         {
-            TaxCalculatorFactory factory = new TaxCalculatorFactory();
+            //TaxCalculatorFactory factory = new TaxCalculatorFactory();
+            //TaxCalculatorFactory factory = TaxCalculatorFactory.GetInstance();
+            TaxCalculatorFactory factory = TaxCalculatorFactory.Instance;
+            Console.WriteLine($"Instance of factory in Main {factory.GetHashCode()}");
             BillingSystem billingSystem = new BillingSystem(factory.CreateTaxCalculator());
+            //BillingSystem billingSystem = new BillingSystem(TaxCalculatorFactory.CreateTaxCalculator());
+            billingSystem.GenerateBill();
+            //POS();
+        }
+
+        // client 2
+        public static void POS()
+        {
+            //TaxCalculatorFactory factory = new TaxCalculatorFactory();
+            //TaxCalculatorFactory factory = TaxCalculatorFactory.GetInstance();
+            TaxCalculatorFactory factory = TaxCalculatorFactory.Instance;
+            Console.WriteLine($"Instance of factory in POS() {factory.GetHashCode()}");
+            BillingSystem billingSystem = new BillingSystem(factory.CreateTaxCalculator());
+            //BillingSystem billingSystem = new BillingSystem(TaxCalculatorFactory.CreateTaxCalculator());
             billingSystem.GenerateBill();
         }
     }
 
 
 
-    class TaxCalculatorFactory
+    class TaxCalculatorFactory // OCP  - Singleton
     {
-        public ITaxCalculator CreateTaxCalculator()
+
+        // 1. don't allow clients to create an instance - private/protected ctor
+
+        protected TaxCalculatorFactory()
+        {
+
+        }
+
+        // 2. internalize the object creation - self obj creation
+
+        public static readonly TaxCalculatorFactory Instance = new TaxCalculatorFactory();
+
+        //private static TaxCalculatorFactory instance = null;
+        //private static object lockObj = new object();
+        //public static TaxCalculatorFactory GetInstance()
+        //{
+        //    lock (lockObj)
+        //    {
+        //        lock (lockObj)
+        //        {
+        //            if (instance == null)
+        //            {
+        //                instance = new TaxCalculatorFactory();
+        //            }
+        //        }
+        //    }
+        //    return instance;
+        //}
+
+        public virtual ITaxCalculator CreateTaxCalculator()
         {
             string className = ConfigurationManager.AppSettings["CALC"];
             // Reflextion
@@ -26,6 +72,14 @@ namespace POSSystem
 
 
             //new className();
+        }
+    }
+
+    class TaxCalcFactory2 : TaxCalculatorFactory
+    {
+        public override ITaxCalculator CreateTaxCalculator()
+        {
+            return base.CreateTaxCalculator();
         }
     }
 
@@ -85,6 +139,47 @@ namespace POSSystem
             int xyz = 20;
             double tax = vat + cess + st + sbt + xyz;
             return tax;
+        }
+    }
+
+    public class APTaxCalculator : ITaxCalculator
+    {
+        //sdfsdfsdf
+        //sdfsdfsdf
+        //sdfsdfsd
+        public double CalculateTax(double amt)
+        {
+            Console.WriteLine("Using AP Tax Calculator");
+            return 120.0;
+        }
+    }
+
+    // vendor : US tax calculator (ustax.dll)
+
+    public class USTaxCalculator
+    {
+        public float ComputeTax(float amt)
+        {
+            //sfsdfsd
+            //sdfsdfd
+            //sdfsdfd
+            //sdfsdf
+            return 120.0f;
+        }
+    }
+
+    public class USTaxCalculatorAdaptor : ITaxCalculator
+    {
+        public double CalculateTax(double amt)
+        {
+            //fsdfsdfsdf
+            //USTaxCalculator calc = new USTaxCalculator();
+            //float tax = calc.ComputeTax((float) amt);
+            //return tax;
+
+            Console.WriteLine("Using US Tax Calculator");
+            return new USTaxCalculator().ComputeTax((float)amt);
+
         }
     }
 }
