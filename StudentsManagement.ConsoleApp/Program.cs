84fdd49513b378861726941e756b1031aa4899ea
Venkat Shiva Reddy <venkatshiva.reddy@gmail.com>
2024-03-12 16:39:58 +0530

    insert sql

--- StudentsManagement.ConsoleApp/Program.cs
+++ StudentsManagement.ConsoleApp/Program.cs
@@ -28,18 +28,27 @@ namespace StudentsManagement.ConsoleApp
             // Framework : ADO.Net
             // Approach: Connected Approach
 
-            SqlConnection conn = new SqlConnection();
-            conn.ConnectionString = @"Server=(localdb)\mssqllocaldb;Database=StudentDB2024;Integrated Security=true";
+
 
             // Step 1: connect to DB
+            SqlConnection conn = new SqlConnection();
+            conn.ConnectionString = @"Server=(localdb)\mssqllocaldb;Database=StudentDB2024;Integrated Security=true";
             conn.Open();
             Console.WriteLine("Connected...");
 
             // Step 2: send sql commonds to db - select/insert/delete/update
+            string sqlInsert = $"insert into students values ('{student.FirstName}','{student.LastName}','{student.Dob}','{student.Email}','{student.Mobile}','{student.Course}')";
+
+            SqlCommand cmd = new SqlCommand(sqlInsert, conn);
+            //cmd.Connection = conn;
+            //cmd.CommandText = sqlInsert;
+            cmd.ExecuteNonQuery();
+
             // Step 3: process the returned data
             // Step 4: close db connection
+            conn.Close();
 
-
+            Console.WriteLine("saved...");
 
 
         }
