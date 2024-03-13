2352a844b385e1206a3bcdd9fdbd55dd5177b297
Venkat Shiva Reddy <venkatshiva.reddy@gmail.com>
2024-03-13 14:56:32 +0530

    ado disconnected approach

--- StudentsManagement.ConsoleApp/PresentationLayer/Program.cs
+++ StudentsManagement.ConsoleApp/PresentationLayer/Program.cs
@@ -3,6 +3,7 @@ using System;
 using System.Configuration;
 using System.Data;
 using System.Data.Common;
+using System.Data.SqlClient;
 
 namespace StudentsManagement.ConsoleApp
 {
@@ -10,13 +11,100 @@ namespace StudentsManagement.ConsoleApp
     {
         static void Main(string[] args)
         {
+            // get all students info - disconnected approach
+
+            SqlConnection sqlConnection = new SqlConnection("Server=(localdb)\\mssqllocaldb;Database=StudentDB2024;Integrated Security=true");
+
+            string selectSql = "select * from students";
+
+            SqlDataAdapter da = new SqlDataAdapter(selectSql, sqlConnection);
+            DataSet ds = new DataSet();
+            //sqlConnection.Open();
+
+            da.Fill(ds, "students");
+
+            //sqlConnection.Close();
+
+            foreach (DataRow row in ds.Tables["students"].Rows)
+            {
+                Console.WriteLine(row["firstName"]);
+                if (row["rollNo"] == "1000")
+                {
+                    //row.Delete(); // delete
+                    row["mobile"] = "1234567"; // update
+                }
+
+
+            }
+
+            // add a new row
+            DataRow newRow = ds.Tables["students"].NewRow();
+            newRow[0] = 123;
+            newRow[1] = "Rajesh";
+
+            ds.Tables["students"].Rows.Add(newRow);
+
+
+            // update
+
+            //da.InsertCommand = new SqlCommand("");
+            //da.DeleteCommand = new SqlCommand("");
+            //da.UpdateCommand = new SqlCommand("");
+
+            SqlCommandBuilder sqlCommandBuilder = new SqlCommandBuilder(da);
+            da.Update(ds, "students");
+        }
+
+
+        public static void TransferMoney(int fromAccNo, int toAccNo, int amount)
+        {
+            SqlConnection sqlConnection = new SqlConnection("Server=(localdb)\\mssqllocaldb;Database=StudentDB2024;Integrated Security=true");
+
+
+            string update1 = $"update accounts set balance = balance - {amount} where accno = {fromAccNo}";
+            string update2 = $"update accounts set balance = balance + {amount} where accno = {toAccNo}";
+
+            SqlCommand cmd1 = new SqlCommand(update1, sqlConnection);
+            SqlCommand cmd2 = new SqlCommand(update2, sqlConnection);
+            sqlConnection.Open();
+            SqlTransaction tx = sqlConnection.BeginTransaction();
+            cmd1.Transaction = tx;
+            cmd2.Transaction = tx;
+
+
+            using (sqlConnection)
+            {
+                try
+                {
+
+                    cmd1.ExecuteNonQuery();
+                    //sdfdsfsdf
+                    //sdfdfdfsdf/
+                    //sdfsdfsdf
+                    //throw new Exception();
+                    cmd2.ExecuteNonQuery();
+
+                    tx.Commit();
+                }
+                catch (Exception ex)
+                {
+                    tx.Rollback();
+                    throw ex;
+                }
+            }
+
+        }
+
+
+        public static void Insert()
+        {
             // collect student data from user and store into database
 
             // in UI
             Student student = new Student
             {
                 //FirstName = "Rohith",
-                FirstName = "db nutral code",
+                FirstName = "with sp",
                 LastName = "Kumar",
                 Dob = DateTime.Parse("01/10/2009"),
                 Email = "rohit@mail.com",
@@ -53,13 +141,15 @@ namespace StudentsManagement.ConsoleApp
             // SQL Injection
 
             //string sqlInsert = $"insert into students values ('{student.FirstName}','{student.LastName}','{student.Dob}','{student.Email}','{student.Mobile}','{student.Course}')";
-
-            string sqlInsert = $"insert into students values (@fname,@lname,@dob,@email,@mobile,@course)";
+            //string sqlInsert = "insert into students values (@fname,@lname,@dob,@email,@mobile,@course)";
+            string sqlInsert = "sp_Insert_Student";
 
             //SqlCommand cmd = new SqlCommand(sqlInsert, conn);
             IDbCommand cmd = conn.CreateCommand();
             cmd.CommandText = sqlInsert;
+            cmd.CommandType = CommandType.StoredProcedure;
             cmd.Connection = conn;
+
             //SqlParameter p1 = new SqlParameter();
             IDbDataParameter p1 = cmd.CreateParameter();
 
@@ -111,6 +201,7 @@ namespace StudentsManagement.ConsoleApp
             }
             //
             Console.WriteLine("saved...");
+
 
 
         }
---
+++ StudentsManagementWindowsFormsApp/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace StudentsManagementWindowsFormsApp
+{
+    public partial class Form1 : Form
+    {
+        SqlConnection sqlConnection = new SqlConnection("Server=(localdb)\\mssqllocaldb;Database=StudentDB2024;Integrated Security=true");
+
+        SqlDataAdapter da = null;
+        DataSet ds = null;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // load data
+
+
+            string selectSql = "select * from students";
+
+            da = new SqlDataAdapter(selectSql, sqlConnection);
+            ds = new DataSet();
+            //sqlConnection.Open();
+
+            da.Fill(ds, "students");
+            dataGridView1.DataSource = ds;
+            dataGridView1.DataMember = "students";
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            // update
+            SqlCommandBuilder sqlCommandBuilder = new SqlCommandBuilder(da);
+            da.Update(ds, "students");
+            MessageBox.Show("Students data updated");
+        }
+    }
+}
