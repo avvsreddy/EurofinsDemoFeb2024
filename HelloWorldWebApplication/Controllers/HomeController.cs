e0503a6dcc7fad0ea397be2f094d0e96ec272625
Venkat Shiva Reddy <venkatshiva.reddy@gmail.com>
2024-03-22 11:15:02 +0530

    MVC

---
+++ HelloWorldWebApplication/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using System.Web.Mvc;
+
+namespace HelloWorldWebApplication.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        // .../home/hello
+        public ActionResult Hello()
+        {
+            string msg = "Hello, Welcome to my first mvc application";
+            ViewBag.Message = msg;
+            return View();
+        }
+    }
+}
