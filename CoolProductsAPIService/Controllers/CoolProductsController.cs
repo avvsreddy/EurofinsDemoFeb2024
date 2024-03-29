f67d6ab4d18a09148c361dc5b7007321241aa256
Venkat Shiva Reddy <venkatshiva.reddy@gmail.com>
2024-03-29 15:22:00 +0530

    api labs

--- CoolProductsAPIService/Controllers/CoolProductsController.cs
+++ CoolProductsAPIService/Controllers/CoolProductsController.cs
@@ -6,6 +6,7 @@ using System.Web.Http;
 
 namespace CoolProductsAPIService.Controllers
 {
+    [RoutePrefix("api/coolproducts")]
     public class CoolProductsController : ApiController
     {
         // design the endpoint / uri
@@ -15,6 +16,8 @@ namespace CoolProductsAPIService.Controllers
 
         ProductsDbContext db = new ProductsDbContext(); // DIP - DOTO: use repository insted of context
 
+        // get all products
+
         [HttpGet]
         public List<Product> GetProducts()
         {
@@ -22,6 +25,49 @@ namespace CoolProductsAPIService.Controllers
             var allProducts = db.Products.ToList();
             return allProducts;
         }
+
+        //client:  get product by id
+        // GET .../api/coolproducts/123
+        public IHttpActionResult GetProductById(int id)
+        {
+            // fetch product by id
+            var product = db.Products.Find(id);
+            if (product == null)
+            {
+                // not found
+                // return http status code 404
+                return NotFound();
+            }
+
+            return Ok(product); // if found then return 200 with data
+        }
+
+
+        // client: get all products based on category
+        // URI - GET .../api/coolproducts/category/mobiles
+        [HttpGet]
+        [Route("category/{category}")]
+        public List<Product> GetProductsByCategory(string category)
+        {
+            var products = db.Products.Where(p => p.Category.Contains(category)).ToList();
+            return products;
+        }
+
+
+        // Lab 1: Get all products based on country
+        // todo: design the url - implement the endpoint - test the endpoint - with http best practics
+
+        // Lab 2: Get all products based on brand
+
+        // Lab 3: Get one cheapest product
+
+        // Lab 4: Get one costliest product
+
+        // Lab 5: Get all products the price between min and max - 50000 100000
+
+        // Lab 6: Get product based on the name
+
+        // Lab 7: GEt all products in the stock
 
 
     }
