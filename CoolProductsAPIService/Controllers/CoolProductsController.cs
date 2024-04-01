5c646b8eb74f916e9875c5a6b8387881644e0f7f
Venkat Shiva Reddy <venkatshiva.reddy@gmail.com>
2024-04-01 13:58:38 +0530

    web api labs

--- CoolProductsAPIService/Controllers/CoolProductsController.cs
+++ CoolProductsAPIService/Controllers/CoolProductsController.cs
@@ -3,6 +3,7 @@ using CoolProductsAPIService.Models.Entities;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
+using System.Web.Http.OData;
 
 namespace CoolProductsAPIService.Controllers
 {
@@ -19,10 +20,12 @@ namespace CoolProductsAPIService.Controllers
         // get all products
 
         [HttpGet]
-        public List<Product> GetProducts()
+        [EnableQuery]
+        //AllowedQueryOptions = System.Web.Http.OData.Query.AllowedQueryOptions.Select | System.Web.Http.OData.Query.AllowedQueryOptions.OrderBy)]
+        public IQueryable<Product> GetProducts()
         {
             // fetch all products from model and return
-            var allProducts = db.Products.ToList();
+            var allProducts = db.Products.AsQueryable<Product>();
             return allProducts;
         }
 
@@ -43,8 +46,8 @@ namespace CoolProductsAPIService.Controllers
         }
 
 
-        // client: get all products based on category
-        // URI - GET .../api/coolproducts/category/mobiles
+        //// client: get all products based on category
+        //// URI - GET .../api/coolproducts/category/mobiles
         [HttpGet]
         [Route("category/{category}")]
         public List<Product> GetProductsByCategory(string category)
@@ -54,21 +57,125 @@ namespace CoolProductsAPIService.Controllers
         }
 
 
-        // Lab 1: Get all products based on country
-        // todo: design the url - implement the endpoint - test the endpoint - with http best practics
+        //// Lab 1: Get all products based on country
+        //// todo: design the url - implement the endpoint - test the endpoint - with http best practics
 
-        // Lab 2: Get all products based on brand
+        [HttpGet]
+        [Route("country/{country}")]
+        public List<Product> GetProductsByCountry(string country)
+        {
+            return db.Products.Where(p => p.Country.Contains(country)).ToList();
+        }
 
-        // Lab 3: Get one cheapest product
+        //// Lab 2: Get all products based on brand
+        [HttpGet]
+        [Route("brand/{brand}")]
+        public List<Product> GetProductsByBrand(string brand)
+        {
+            return db.Products.Where(p => p.Brand.Contains(brand)).ToList();
+        }
 
-        // Lab 4: Get one costliest product
+        //// Lab 3: Get one cheapest product
+        [HttpGet]
+        [Route("cheapest")]
+        public Product GetCheapestProduct()
+        {
+            return db.Products.OrderBy(p => p.Price).FirstOrDefault();
+        }
 
-        // Lab 5: Get all products the price between min and max - 50000 100000
+        //// Lab 4: Get one costliest product
 
-        // Lab 6: Get product based on the name
+        [HttpGet]
+        [Route("costliest")]
+        public Product GetCostliestProduct()
+        {
+            return db.Products.OrderByDescending(p => p.Price).FirstOrDefault();
+        }
+        //// Lab 5: Get all products the price between min and max - 50000 100000
 
-        // Lab 7: GEt all products in the stock
+        //// ... /min/1000/max/5000
+        [HttpGet]
+        [Route("min/{min}/max/{max}")]
+        public List<Product> GetProductsByPriceRange(int min, int max)
+        {
+            return db.Products.Where(p => p.Price >= min && p.Price <= max).ToList();
+        }
+        //// Lab 6: Get product based on the name
 
+        [HttpGet]
+        [Route("name/{name}")]
+        public List<Product> GetProductsByName(string name)
+        {
+            return db.Products.Where(p => p.Name.Contains(name)).ToList();
+        }
+        //// Lab 7: GEt all products in the stock
+        [HttpGet]
+        [Route("instock")]
+        public List<Product> GetProductsByAvailability()
+        {
+            return db.Products.Where(p => p.IsInStock).ToList();
+        }
+
+
+        // end point for adding new data 
+
+        [HttpPost]
+        public IHttpActionResult Post([FromBody] Product product)
+        {
+            // validate
+            if (product == null)
+            {
+                return BadRequest("Invalid or empty product details posted");
+            }
+
+            if (!ModelState.IsValid)
+            {
+                return BadRequest("Invalid product details posted");
+            }
+
+            db.Products.Add(product);
+            db.SaveChanges();
+
+            // return status code 201 + location + saved data
+            return Created($"/api/products/{product.Id}", product);
+        }
+        // .../api/coolproducts/1
+        // Delete endpoint
+        [HttpDelete]
+        public IHttpActionResult Delete(int id)
+        {
+            // is product found
+            var productToDel = db.Products.Find(id);
+            if (productToDel == null)
+            {
+                return NotFound();
+            }
+            db.Products.Remove(productToDel);
+            db.SaveChanges();
+            return Ok();
+        }
+
+        // edit
+        [HttpPut]
+        public IHttpActionResult EditProduct([FromBody] Product product)
+        {
+            // validate
+            if (product == null)
+            {
+                return BadRequest("Invalid or empty product details posted");
+            }
+
+            if (!ModelState.IsValid)
+            {
+                return BadRequest("Invalid product details posted");
+            }
+
+            //db.Products.Add(product);
+            db.Entry(product).State = System.Data.Entity.EntityState.Modified;
+            db.SaveChanges();
+
+            return Ok();
+        }
 
     }
 }
---
+++ CoolProductsClientConsoleApp/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+
+namespace CoolProductsClientConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // fetch the list of products from web api and display
+
+            // Discover the URL of the api
+            string url = "https://localhost:44324/api/CoolProducts";
+
+            HttpClient client = new HttpClient();
+
+
+            var request = client.GetAsync(url);
+            //sdfsdfsdf
+            //sdfsdfsdf
+            //sdfsdfsdf
+            var result = request.Result;
+
+            if (result.IsSuccessStatusCode)
+            {
+                string data = result.Content.ReadAsStringAsync().Result;
+                var items = result.Content.ReadAsAsync<List<Item>>().Result;
+
+                foreach (var item in items) { Console.WriteLine(item.Name); }
+                //Console.WriteLine(item.Name);
+            }
+        }
+
+        // Root myDeserializedClass = JsonConvert.DeserializeObject<Root>(myJsonResponse);
+        public class Item
+        {
+            public int Id { get; set; }
+            public string Name { get; set; }
+            public string Description { get; set; }
+            public string Category { get; set; }
+            public int Price { get; set; }
+            public string Brand { get; set; }
+            public string Country { get; set; }
+            public bool IsInStock { get; set; }
+        }
+
+
+    }
+}
